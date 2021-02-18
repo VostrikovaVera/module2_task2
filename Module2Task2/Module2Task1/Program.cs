8f657a27224358bfb6af9a332a9ee02a16a06158
Vira Vostrikova <vvostrikova@csod.com>
2021-02-18 23:17:57 +0200

    Empty project added

---
+++ Module2Task2/Module2Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Module2Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var starter = new Starter();
+            starter.Run();
+        }
+    }
+}
---
+++ Module2Task2/Module2Task1/Starter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace Module2Task1
+{
+    public class Starter
+    {
+        private readonly int _minRandomAction = 0;
+        private readonly int _maxRandomAction = 3;
+        private readonly Random _random = new Random();
+        private readonly Actions actions = new Actions();
+        private readonly Logger logger = Logger.Instance;
+
+        public void Run()
+        {
+
+            for (var i = 0; i < 100; i++)
+            {
+                var actionResult = new Result();
+                var actionNumber = _random.Next(_minRandomAction, _maxRandomAction);
+
+                switch (actionNumber)
+                {
+                    case 0:
+                        actionResult = actions.InfoAction();
+                        break;
+                    case 1:
+                        actionResult = actions.WarningAction();
+                        break;
+                    case 2:
+                        actionResult = actions.ErrorAction();
+                        break;
+                };
+
+                if (!actionResult.Status)
+                {
+                    var logMessage = $"Action failed by a reason: {actionResult.ErrMessage}";
+                    logger.LogEvent(LogTypes.Error, logMessage);
+                }
+            }
+
+            File.WriteAllText("log.txt", logger.GeneralLog.ToString());
+        }
+    }
+}
